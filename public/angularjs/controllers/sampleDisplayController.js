875587e36c65ff78c2d64c889cdefa2a64ea1034
Mayank Tanwa <mayank.tanwar@sjsu.edu>
2016-12-19 22:12:57 -0800

    foodCoin with mozdef automated incident detection

---
+++ public/angularjs/controllers/sampleDisplayController.js
@@ -0,0 +1,65 @@
+
+
+index.controller('sampleDisplayController', function($scope, $http, $state, $anchorScroll,NgMap) {
+	
+	
+	
+	
+	
+	
+	
+	/*NgMap.getMap().then(function(map) {
+        console.log(map.getCenter());
+        console.log('markers', map.markers);
+        console.log('shapes', map.shapes);
+    });
+
+
+    $scope.showproperty = function(e,p){
+
+        $scope.p = p;
+        $scope.showInfoWindow('bar', p._id);
+        $scope.limage = p.listingImages[0][0];
+        console.log("Image URL:"+$scope.limage);
+    };*/
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	/*angular.element("#infinite-scroll-container").on('scroll', handler);
+	angular.element("#infinite-scroll-container").height()*/
+	
+	/*var $animation_elements = angular.element('.animation-element');
+	var $window = angular.element(window);
+
+	function check_if_in_view() {
+	  var window_height = $window.height();
+	  var window_top_position = $window.scrollTop();
+	  var window_bottom_position = (window_top_position + window_height);
+	 
+	  $.each($animation_elements, function() {
+	    var $element = $(this);
+	    var element_height = $element.outerHeight();
+	    var element_top_position = $element.offset().top;
+	    var element_bottom_position = (element_top_position + element_height);
+	 
+	    //check to see if this current container is within viewport
+	    if ((element_bottom_position >= window_top_position) &&
+	        (element_top_position <= window_bottom_position)) {
+	      $element.addClass('in-view');
+	    } else {
+	      $element.removeClass('in-view');
+	    }
+	  });
+	}
+
+	$window.on('scroll resize', check_if_in_view);
+	$window.trigger('scroll');*/
+	
+});
